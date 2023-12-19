d5abf8548450901e28367b447b4462516afabeeb
nascal3 <nascal3@gmail.com>
2023-12-19 17:57:52 +0300

    added resolver

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+// Import DB
+import db from './_db.js'
+
 // Import types
 import { typeDefs } from "./schema.js";
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        authors() {
+            return db.authors
+        },
+        reviews() {
+            return db.reviews
+        }
+    }
+}
 
 // Server setUp
 const server = new ApolloServer({
     // typeDefs (type definitions)
-    typeDefs
+    typeDefs,
     //resolvers
-
+    resolvers
 })
 
 const { url } = await startStandaloneServer(server, {
